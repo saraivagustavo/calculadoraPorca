782e1ac33229732c94d718aa1408c89e96a9425a
Gustavo Saraiva <gsaraivam10@gmail.com>
2025-08-20 16:04:12 -0300

    Exceção especificada em "src/.../model/Calculadora.java"

--- src/main/java/br/ifes/ps/calculadora/model/Calculadora.java
+++ src/main/java/br/ifes/ps/calculadora/model/Calculadora.java
@@ -1,9 +1,9 @@
 package br.ifes.ps.calculadora.model;
 
 public class Calculadora {
-    public int calcular(Operacao operacao, int valor1, int valor2) throws Exception {
+    public int calcular(Operacao operacao, int valor1, int valor2) throws IllegalArgumentException {
         if (operacao == null) {
-            throw new Exception("Operação não pode ser nula");
+            throw new IllegalArgumentException("Operação não pode ser nula");
         }
 
        return operacao.execute(valor1, valor2);
