5d2e822d2ad72b682d559df0d41850c2b348bc3e
Gustavo Saraiva <124311748+saraivagustavo@users.noreply.github.com>
2025-08-20 15:35:41 -0300

    Delete CalculadoraMenosPorca directory

--- CalculadoraMenosPorca/src/main/java/br/ifes/ps/calculadora/model/Operacao.java
+++
@@ -1,6 +0,0 @@
-package br.ifes.ps.calculadora.model;
-
-public interface Operacao {
-
-    public int execute (int a, int b);
-}
--- CalculadoraMenosPorca/src/main/java/br/ifes/ps/calculadora/model/Subtrair.java
+++
@@ -1,8 +0,0 @@
-package br.ifes.ps.calculadora.model;
-
-public class Subtrair implements  Operacao {
-    @Override
-    public int execute(int a, int b) {
-        return a-b;
-    }
-}
