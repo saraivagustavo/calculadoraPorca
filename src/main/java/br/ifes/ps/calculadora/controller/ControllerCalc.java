888a3114a85840f14cae58e0444df40e5579dded
Gustavo Saraiva <gsaraivam10@gmail.com>
2025-08-20 16:20:09 -0300

    Atualização na verificação da opção

--- src/main/java/br/ifes/ps/calculadora/controller/ControllerCalc.java
+++ src/main/java/br/ifes/ps/calculadora/controller/ControllerCalc.java
@@ -4,9 +4,8 @@ import br.ifes.ps.calculadora.model.Calculadora;
 import br.ifes.ps.calculadora.model.Operacao;
 
 public class ControllerCalc {
-    public int operacaoCalcular(Operacao operacao, int valor1, int valor2) throws Exception {
+    public int operacaoCalcular(Operacao operacao, int valor1, int valor2){
         Calculadora calculadora = new Calculadora();
-        int resultado = calculadora.calcular(operacao, valor1, valor2);
-        return resultado;
+        return calculadora.calcular(operacao, valor1, valor2);
     }
 }
